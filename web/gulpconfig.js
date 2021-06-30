e7984405e6ee507aa02b984a87fab96e01ab03e6
FLY <jinfei@outlook.com>
2021-06-30 14:17:43 +0800

    test

--- web/gulpconfig.js
+++ web/gulpconfig.js
@@ -1,3 +1,11 @@
 module.exports = {
-     
+     "f": {
+         'host': '172.16.92.11',
+         'user': 'root',
+         'pass': 'Zone2020@)@)',
+         "port": 22,
+         "remotePath": "/data/o2server/servers/webServer/",
+         "upload": "sftp",
+         "dest": "dest"
+     }
 };
